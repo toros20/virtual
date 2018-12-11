37b338b03d69e70d9e2069c58c3d149254a58dca
MARVIN TORO <sistemas@sanjosedelcarmen.edu.hn>
2018-12-11 11:03:59 -0600

    cambio de titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>VIRTUAL LMS  2.0</title>
+        <title>VIRTUAL LMS  2.0 MARVIN TORO</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
