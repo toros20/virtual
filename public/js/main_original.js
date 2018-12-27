958b1517fcc9f3c39bdaf05cc402be0d67a37d2e
MARVIN TORO <sistemas@sanjosedelcarmen.edu.hn>
2018-12-26 21:55:16 -0600

    se ha instalado las depe NPM, ademas de la plantilla web y sus librerias

---
+++ public/js/main_original.js
@@ -0,0 +1,107 @@
+function loadcourses(){
+
+    //obtenemos  el id de la modalidad seleccionada
+     var modality = $("#modalities").val();
+     var token = $("#token").val();
+
+    $.ajax({
+
+        url:'../../ajax/coursesbymodalityid',
+        headers: token ,
+        data: {modality_id:modality,_token:token},
+        type:'POST',
+        datatype:'json',
+        success:function(data)
+        {
+            //console.log(response);
+            $('#courses').html(data);
+        },
+        error: function (response) {
+            console.log(response);
+          }
+    });
+   
+
+}
+
+//codigo para cargar solo las secciones asignadas al curso seleccionado
+function loadsections(){
+
+    //obtenemos  el id de la modalidad seleccionada
+     var course = $("#courses").val();
+     var token = $("#token").val();
+    
+    $.ajax({
+
+        url:'../../ajax/sectionsbycoursesid',
+        headers: token ,
+        data: {course_id:course,_token:token},
+        type:'POST',
+        datatype:'json',
+        success:function(data)
+        {
+            //console.log(response);
+            $('#sections').html(data);
+        },
+        error: function (response) {
+            console.log(response);
+          }
+    });
+   
+
+}
+
+//codigo para cargar solo las clases asignadas al curso seleccionado
+function loadclases(){
+
+    //obtenemos  el id de la modalidad seleccionada
+     var course = $("#courses").val();
+     var token = $("#token").val();
+    
+    $.ajax({
+
+        url:'../../ajax/clasesbycoursesid',
+        headers: token ,
+        data: {course_id:course,_token:token},
+        type:'POST',
+        datatype:'json',
+        success:function(data)
+        {
+            //console.log(response);
+            $('#clases').html(data);
+        },
+        error: function (response) {
+            console.log(response);
+          }
+    });
+   
+
+}
+
+//codigo para cargar solo las clases asignadas a una modalidad seleccionado
+function loadclasesbymodality(){
+
+    //obtenemos  el id de la modalidad seleccionada
+     var modalidad = $("#modalities").val();
+     var token = $("#token").val();
+    
+    $.ajax({
+
+        url:'../../ajax/clasesbymodalityid',
+        headers: token ,
+        data: {modality_id:modalidad,_token:token},
+        type:'POST',
+        datatype:'json',
+        success:function(data)
+        {
+            //console.log(response);
+            $('#clases').html(data);
+        },
+        error: function (response) {
+            console.log(response);
+          }
+    });
+   
+
+}
+
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,51 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js(
+   [
+
+     'resources/js/app.js',
+     'resources/js/jquery-3.2.1.js',
+     'resources/js/jquery.appear.js',
+     'resources/js/jquery.mousewheel.js',
+     'resources/js/perfect-scrollbar.js',
+     'resources/js/jquery.matchHeight.js',
+     'resources/js/svgxuse.js',
+     'resources/js/imagesloaded.pkgd.js',
+     'resources/js/Headroom.js',
+     'resources/js/velocity.js',
+     //'resources/js/ScrollMagic.js',
+     'resources/js/jquery.waypoints.js',
+     'resources/js/jquery.countTo.js',
+     'resources/js/popper.min.js',
+     'resources/js/material.min.js',
+     'resources/js/bootstrap-select.js',
+     'resources/js/smooth-scroll.js',
+     'resources/js/selectize.js',
+     'resources/js/swiper.jquery.js',
+
+     //'resources/js/moment.js',
+    // 'resources/js/daterangepicker.js',
+     'resources/js/simplecalendar.js',
+     //'resources/js/fullcalendar.js',
+     'resources/js/isotope.pkgd.js',
+     'resources/js/ajax-pagination.js',
+     'resources/js/Chart.js',
+     'resources/js/chartjs-plugin-deferred.js',
+     'resources/js/circle-progress.js',
+     'resources/js/loader.js',
+     'resources/js/run-chart.js',
+     'resources/js/jquery.magnific-popup.js',
+     'resources/js/jquery.gifplayer.js',
+     //'resources/js/mediaelement-and-player.js',
+     //'resources/js/mediaelement-playlist-plugin.min.js',
+     'resources/js/ion.rangeSlider.js',
+     'resources/js/base-init.js',
+     'resources/fonts/fontawesome-all.js',
+     'resources/js/bootstrap.bundle.min.js'
+     //'resources/js/bootstrap.min.js'
+
+   ] , 'public/js/app.js');
+
+
+   //.sass('resources/sass/app.scss', 'public/css/app.css');
