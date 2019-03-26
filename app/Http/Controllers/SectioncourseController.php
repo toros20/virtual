011acae69d1af102461ecbbd70f3b842635f9b9f
Marvin <marvinlink07@gmail.com>
2019-03-26 15:07:31 -0600

    crear taks tables in sectioncourses

--- app/Http/Controllers/SectioncourseController.php
+++ app/Http/Controllers/SectioncourseController.php
@@ -7,6 +7,10 @@ use App\Course;
 use App\Sectioncourse;
 use Illuminate\Http\Request;
 
+use Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
 class SectioncourseController extends Controller
 {
     /**
@@ -40,8 +44,49 @@ class SectioncourseController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+    {   
+        //creamos la nueva asignacion de seccion a curso
         Sectioncourse::create($request->all());
+
+        //obtenemos el curso y seccion
+        $seccion=strtolower($request->section);
+        $curso = $request->course_id;
+
+        //nombramos las tablas que vamos a crear
+        $tbl_task='task_'.$curso.'_'.$seccion;
+        $tbl_taskstudent='taskstudent_'.$curso.'_'.$seccion;
+
+        //creamos la tabla task principal de este curso y seccion (ejemplo task_1_a)
+        Schema::connection('mysql')->create($tbl_task, function($table)
+        {
+            $table->increments('id');
+            $table->integer('teacher');
+            $table->integer('clase');
+            $table->integer('parcial');
+            $table->string('titulo');
+            $table->text('descripcion');
+            $table->boolean('evaluada');
+            $table->date('fecha_entrega');
+            $table->date('fecha_publicada');
+            $table->integer('valor');
+            $table->string('tipo');
+
+            $table->timestamps();
+        });
+
+
+        //creamos la tabla taskstudents de este curso y seccion (ejemplo taskstudent_1_a)
+        Schema::connection('mysql')->create( $tbl_taskstudent, function($table) use ($curso,$seccion)
+        {
+            $table->increments('id');
+            $table->integer('student');
+            $table->integer( 'task_'.$curso.'_'.$seccion.'_id');
+            $table->integer('valor_obtenido');
+            $table->string('observacion')->nullable($value = true);
+
+            $table->timestamps();
+        });
+
         return redirect()->route('sectioncourses.index');
     }
 
