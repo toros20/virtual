ea4be895efd72b82637084749f63eca0eefe0979
Marvin <marvinlink07@gmail.com>
2019-04-08 12:30:09 -0600

    auth.login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -25,7 +25,7 @@ class RedirectIfAuthenticated
       
         if (Auth::guard($guard)->check()) {
            // return redirect('/home');
-           return view('login');
+           return view('auth.login');
            
         }
 
