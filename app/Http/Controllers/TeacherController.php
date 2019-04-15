4e7ad169df04fca5e1677b09bbeb068ceb836420
Marvin <marvinlink07@gmail.com>
2019-04-15 08:53:46 -0600

    prueba

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -643,8 +643,8 @@ class TeacherController extends Controller
                             )
                         ->orderBy('users.sexo', 'asc')
                         ->orderBy('users.name', 'asc')
-                        ->get();
-        //dd($students);
+                        ->ToSql();
+        dd($students);
        
         return view('teachers/examen',compact(
             'user','clase_actual','curso_actual','section_actual','asignaciones','students'));
