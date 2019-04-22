dfaa60b61bc40aafd4cfe1e9a8dc941aa05db12e
Marvin <marvinlink07@gmail.com>
2019-04-22 10:53:05 -0600

    boletas

--- resources/views/users/panel.blade.php
+++ resources/views/users/panel.blade.php
@@ -144,7 +144,7 @@
                         <div class="card-body card-body-cascade ">
                                         
                             <ul class="list-group">
-                                    <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('users/boletas')}} ">
+                                    <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('users_boletas')}} ">
                                         <li class="list-group-item">
                                             <div class="md-v-line"></div><i class="fas fa-user mr-5"></i>Boletas
                                         </li>
