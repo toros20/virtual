9d220ff0808af99def595bac7ebd9e48551630fb
Marvin <marvinlink07@gmail.com>
2019-04-22 12:36:18 -0600

    boletas

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -28,67 +28,8 @@
 
     <!--Double navigation-->
     <header>
-        <!-- Sidebar navigation -->
-        <div id="slide-out" class="side-nav sn-bg-4">
-          <ul class="custom-scrollbar">
-            <!-- Logo -->
-            <li>
-              <div class="logo-wrapper waves-light">
-                <a href="#"><img style="height: 100px;" src="{{ URL::asset('img/logo-sanjose.PNG')}}" class="img-fluid flex-center"></a>
-              </div>
-            </li>
-            
-            <li>
-              <ul class="collapsible collapsible-accordion">
-                  <ul>
-                      <li>
-                       <p align=center> Navegación</p>
-                      </li>
-                      <hr>
-                     
-                        <li>
-                            <a onclick="boletas()">
-                                Boletas
-                            </a>
-                        </li>
-                     
-                    </ul>
-              </ul>
-            </li>
-            <!--/. Side navigation links -->
-          </ul>
-          <div class="sidenav-bg mask-strong"></div>
-        </div>
-        <!--/. Sidebar navigation -->
-        <!-- Navbar -->
-        <nav class="navbar fixed-top navbar-toggleable-md navbar-expand-lg scrolling-navbar double-nav">
-          <!-- SideNav slide-out button -->
-          <div class="float-left">
-             <a href="#" data-activates="slide-out" class="button-collapse"><i class="fas fa-bars"></i></a> 
-          </div>
-          <!-- Breadcrumb-->
-          <div class="breadcrumb-dn mr-auto">
-             <p>Menú de Navegación</p> 
-          </div>
-          <ul class="nav navbar-nav nav-flex-icons ml-auto">
-            
-             
-            <li class="nav-item">
-              <a class="nav-link"> <span class="clearfix d-none d-sm-inline-block"> Bienvenido {{ $user->name }}</span></a>
-            </li>
-            
-            <li class="nav-item">
-                
-                  <form action="{{ route('logout') }}" method="POST">
-                      <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
-                      <button class="btn btn-sm btn-danger" type="submit">SALIR</button>
-                  </form>
-              {{-- <a class="nav-link"> <span class="clearfix d-none d-sm-inline-block"> SALIR</span></a> --}}
-            </li>
-            
-          </ul>
-        </nav>
-        <!-- /.Navbar -->
+      
+       
     </header>
     <!--/.Double navigation-->
   
@@ -97,32 +38,7 @@
       <div class="container-fluid">
         <!--row-->
         <div class="row"> 
-            <form action="">
-
-                <p><label for="modality_id">
-                        Seleccione Modalidad 
-                         <select name="modality_id" id="modalities" onchange="loadcourses()">
-                                <option value= -1 >Seleccione Modalidad</option>
-                            @foreach ($modalities as $modality)
-                                <option value= {{ $modality->id }} > {{ $modality->name }}</option>
-                            @endforeach
-                        </select>
-                    </label></p>
-
-                    <p><label for="course_id">
-                        Seleccione Curso 
-                         <select name="course_id" id="courses" onchange="loadsections()">
-                            <option>Seleccione Curso</option>
-                        </select>
-                    </label></p>
-
-                    <p><label for="sections" >
-                        Seleccione Secci&oacute;n 
-                         <select name="section" id="sections" >
-                            <option>Seleccione Secci&oacute;n</option>
-                        </select>
-                    </label></p>
-            </form>                
+           Bienvenido a la seccion de boletas           
         </div><!--row-->
 
      </main>
