e13a94a2e0c05adac09d24a7d4454d7d11e8652b
Marvin <marvinlink07@gmail.com>
2019-04-24 15:46:29 -0600

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -836,7 +836,7 @@ class UserController extends Controller
                         ->Select('clase_id')
                         ->get(); 
         
-       $pdf = PDF::loadView('users/boletas',[ [ 'estudiantes' => $estudiantes],  ['curso' => $curso] ,  ['seccion' => $seccion] ,  ['course' => $course] ,  ['section' => $section] ]);
+       $pdf = PDF::loadView('users/boletas', [ 'estudiantes' => $estudiantes],  ['curso' => $curso] ,  ['seccion' => $seccion] ,  ['course' => $course] ,  ['section' => $section] );
     
        return $pdf->stream();
              
