afcd7fa0f9ebca81c682e5d133a356a53dca5bce
Marvin <marvinlink07@gmail.com>
2019-04-25 16:44:32 -0600

    red

--- resources/views/users/actas.blade.php
+++ resources/views/users/actas.blade.php
@@ -148,9 +148,9 @@
                                      @endforeach {{--fin del ciclo para cada clase --}}
                                         
                                      @if ( $reprobadas > 0)
-                                        <td style="font-size:1rem ;text-align:center; width:50px;font-weight: bold; border: 1px solid #dee2e6; color:red"><?php echo $reprobadas ?> </td>
+                                        <td style="font-size:1.25rem ;text-align:center; width:50px;font-weight: bold; border: 1px solid #dee2e6; color:red"><?php echo $reprobadas ?> </td>
                                      @else 
-                                        <td style="font-size:1rem ;text-align:center; width:50px;font-weight: bold; border: 1px solid #dee2e6;"><?php echo $reprobadas ?> </td>
+                                        <td style="font-size:1.25rem ;text-align:center; width:50px;font-weight: bold; border: 1px solid #dee2e6;"><?php echo $reprobadas ?> </td>
                                      @endif
                                             
                                     
