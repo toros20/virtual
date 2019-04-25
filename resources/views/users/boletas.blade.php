71d243b35bda5aec57ad1338a69dd882394e7bbc
Marvin <marvinlink07@gmail.com>
2019-04-24 23:59:51 -0600

    table

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -82,7 +82,7 @@
                         <table  style="margin-top:10px; text-align:center; " border='1' align="center" width="700">
                             <tr><td ><h4 style="font-weight: bold;"> {{$estudiante->name}} {{$estudiante->lastname}}</h4></td></tr>
                         </table>
-                        <table class="table">
+                        <table class="table"  style="margin-top:10px; " border='1' align="center" width="700">
                                 <tr><th style="text-align:center; width:5px;font-weight: bold;">No.</th>
                                     <th style="font-weight: bold;">ESPACIOS PEDAGOGICOS</th>
                                     <th style="text-align:center; width:50px;font-weight: bold;">I P</th>
@@ -93,7 +93,7 @@
                                     <th style="text-align:center; width:50px;font-weight: bold;">RECU.</th>
                                 </tr>
                         </table>
-                        <table  style="margin-top:10px; " border='1' align="center" width="700"> 
+                        <table class="table" style="margin-top:10px; " border='1' align="center" width="700"> 
                         @foreach ($clases as $clase)
 
                         <?php 
