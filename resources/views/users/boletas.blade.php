9dae6c633faf044cf7f2b5709987aeb677182fc9
Marvin <marvinlink07@gmail.com>
2019-04-24 21:31:31 -0600

    test

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -308,7 +308,7 @@
 //==============================================================
 //==============================================================
 
-require_once "../storage/include/mpdf/mpdf.php)";
+require_once (" ../storage/include/mpdf/mpdf.php");
 
 $html = ob_get_clean();
 
