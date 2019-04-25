672b7ecdedd2100606895ec77dc16a4506c55eb3
Marvin <marvinlink07@gmail.com>
2019-04-24 23:08:50 -0600

    css

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -322,7 +322,7 @@ $mpdf->allow_charset_conversion= true;
 
 $mpdf->charset_in='UTF-8';
 
-$mpdf->SetDisplayMode('fullpage');
+//$mpdf->SetDisplayMode('fullpage');
 
 $mpdf->list_indent_first_level = 0; // 1 or 0 - whether to indent the first level of a list
 
