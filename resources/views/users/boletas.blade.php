05843f8ec724ca960a9789276e6201f06944504d
Marvin <marvinlink07@gmail.com>
2019-04-24 22:30:03 -0600

    meta utf-8

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -12,6 +12,7 @@
 
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>Plataforma ISJC</title>
