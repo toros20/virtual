be4a264724e9bef9c8fc6755c3b6ea8ae9b3a8cb
Marvin <marvinlink07@gmail.com>
2019-04-25 11:29:21 -0600

    is bilingue

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -56,21 +56,26 @@
                 
                 @foreach ($estudiantes as $estudiante)
                         <?php $cont=0; $total1=0; $total2=0; $total3=0; $total4=0; ?>
-                      
+                        <tr> <td width="12%" rowspan="2" align="center">
+                                <img src="{{ URL::asset('img/logo_sanjose.png')}}" width="70" height="70" alt=""/>
+                            </td>
+                            <td>
+                                @if ($course->is_bilingue == 1)
+                                    <h4 style="font-weight: bold;">C.E.M.N.G.B. SAN JOSÉ DEL CARMEN </h4>
+                                @else 
+                                    <h4 style="font-weight: bold;">C.E.M.N.G SAN JOSÉ DEL CARMEN </h4>
+                                @endif
+                            <p style="margin-bottom:0px; ">Colonia La Camapaña, Tegucigalpa</p>
+                            <p style="margin-bottom:0px; ">Tel:(+504) 2221-4474 /75</p>
+                            <p style="margin-bottom:0px; ">Web: sanjosedelcarmen.edu.hn</p>
+                            
+                        </td></tr>
                         <table class=" tabla tabla-striped tabla-bordered" style=" text-align:center " align="center" width="700">
-                            <tr> <td width="12%" rowspan="2" align="center">
-                                    <img src="{{ URL::asset('img/logo_sanjose.png')}}" width="70" height="70" alt=""/>
-                                </td>
-                                <td>
-                                <h4 style="font-weight: bold;">C.E.M.N.G SAN JOSÉ DEL CARMEN </h4>
-                                <p style="margin-bottom:0px; ">Colonia La Camapaña, Tegucigalpa</p>
-                                <p style="margin-bottom:0px; ">Tel:(+504) 2221-4474 /75</p>
-                                <p style="margin-bottom:0px; ">Web: sanjosedelcarmen.edu.hn</p>
-                                
-                            </td></tr>
+                            
+                            
                         </table>
                         <table  style="tabla-striped text-align:center; border: 1px solid #dee2e6; "  align="center" width="700">
-                            <tr style="border: 1px solid #dee2e6; "> <td style=" border: 1px solid #dee2e6;"><h5>BOLETA DE CALIFICACIONES</h5></td>
+                            <tr style="border: 1px solid #dee2e6; background-color:#f2f2f2;"> <td style=" border: 1px solid #dee2e6;"><h5>BOLETA DE CALIFICACIONES</h5></td>
                                 <td style=" border: 1px solid #dee2e6;"><?php echo $arrayDias[date('w')]." ".date('d')."/".$arrayMeses[date('m')-1]."/".date('Y');?></td></tr>
                             <tr style="border: 1px solid #dee2e6; ">
                                 <td style="padding-top:5px;  border: 1px solid #dee2e6;">
@@ -114,7 +119,7 @@
                                 $total4+=($resultado[0]->Acum4) + ($resultado[0]->Exa4);
                               
                         ?>
-                            
+                            {{-- Codigo para intercalar por colores las filas --}}
                             @if ($cont%2==0 ) 
                                 <tr style="border: 1px solid #dee2e6; background-color:#f2f2f2;">
                                     <td style="border: 1px solid #dee2e6; font-weight: bold; width:5px; padding:0.35rem;"><?php echo $cont+1; ?></td>
