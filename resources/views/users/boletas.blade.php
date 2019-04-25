2be8b382784e4ea02e6b79716bb9c2e4b7fe7e4a
Marvin <marvinlink07@gmail.com>
2019-04-24 21:44:11 -0600

    link

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -308,7 +308,7 @@
 //==============================================================
 //==============================================================
 
-include("https://sanjosedelcarmen.edu.hn/virtual/storage/include/mpdf/mpdf.php");
+include($_SERVER['DOCUMENT_ROOT']."/storage/include/mpdf/mpdf.php");
 
 $html = ob_get_clean();
 
