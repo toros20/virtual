ff06425ca6c0c4fec5fcbc3f8ef940fd739be87b
Marvin <marvinlink07@gmail.com>
2019-04-24 22:27:25 -0600

    bootstrap

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -20,6 +20,7 @@
   <!-- Bootstrap core CSS -->
 
   <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link href="{{ URL::asset('css/mdb.min.css')}}" rel="stylesheet">
   <!-- Your custom styles (optional) css-->
   <link href="{{ URL::asset('css/style.css')}}" rel="stylesheet">
