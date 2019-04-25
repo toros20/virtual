e07ca5ffff2ddf7d562f83382ce9538ca6990570
Marvin <marvinlink07@gmail.com>
2019-04-25 13:13:51 -0600

    espacio

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -404,9 +404,7 @@
 
                                     <tr><td>&nbsp;</td><td>&nbsp;</td> <td>&nbsp;</td></tr> 
                                     <tr><td>&nbsp;</td><td>&nbsp;</td> <td>&nbsp;</td></tr> 
-                                    <tr><td>&nbsp;</td><td>&nbsp;</td> <td>&nbsp;</td></tr> 
-                                    <tr><td>&nbsp;</td><td>&nbsp;</td> <td>&nbsp;</td></tr> 
-                        
+                                    <tr><td>&nbsp;</td><td>&nbsp;</td> <td>&nbsp;</td></tr>                       
                                 
                                     <tr align="center">
                                             <td >&nbsp;</td>
