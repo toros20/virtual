dd7799c68ac765e9b410c56b23962a2dff4be8fc
Marvin <marvinlink07@gmail.com>
2019-04-24 23:04:18 -0600

    css

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -315,7 +315,7 @@ $html = ob_get_clean();
 $mpdf=new \Mpdf\Mpdf([
     'mode' => 'utf-8',
     'format' => [190, 236],
-    'orientation' => 'L'
+    'orientation' => 'P'
 ]); 
 
 $mpdf->allow_charset_conversion= true;
