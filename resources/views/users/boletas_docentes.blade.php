56b4d4c207d9453d859a28c0417e27a0c66e0040
Marvin <marvinlink07@gmail.com>
2019-04-26 23:39:08 -0600

    totales

--- resources/views/users/boletas_docentes.blade.php
+++ resources/views/users/boletas_docentes.blade.php
@@ -160,6 +160,25 @@
 
                         </table>
 
+                        <table>
+                            <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+                            <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>                            
+                        </table>
+
+                        <table class="tabla tabla-striped tabla-bordered"  style="margin-top:10px; border: 1px solid #dee2e6; "  align="center" width="700">
+                            <tr style="border: 1px solid #dee2e6; background-color:#fbfbfb;">
+                                    <td style="border: 1px solid #dee2e6; font-weight: bold; width:5px; padding:0.35rem;">*</td>
+                                    <td style="border: 1px solid #dee2e6; text-align:left;padding:0.35rem;">Total de Evaluaaciones Realizadas</td>
+                                
+                                    <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;">0</td>
+                                    <td style="text-align:center; width:50px ;padding:0.35rem; border: 1px solid #dee2e6;">0</td> 
+                                    <td style="text-align:center; width:50px ;padding:0.35rem; border: 1px solid #dee2e6;"></td>
+                                    <td style="text-align:center; width:50px ;padding:0.35rem; border: 1px solid #dee2e6;">0</td>
+                                    <td style="text-align:center; width:50px ;padding:0.35rem; border: 1px solid #dee2e6;">0</td>
+                            </tr>
+                        </table>
+
+
                         <table class="tabla tabla-striped tabla-bordered"  style="margin-top:10px; border: 1px solid #dee2e6; "  align="center" width="700">
                                 <tr style="border: 1px solid #dee2e6; ">
                                     <th style="text-align:center; width:5px;font-weight: bold;">No. </th>
@@ -183,7 +202,7 @@
                                             </tr>
                                             <tr>
                                                     <td style="text-align:center; width:5px;font-weight: bold; border:1px solid #dee2e6;">5</td>
-                                                    <td style="font-weight: bold; border:1px solid #dee2e6;">.tal</td>
+                                                    <td style="font-weight: bold; border:1px solid #dee2e6;">.</td>
                                                 </tr>
                         </table>
                         <pagebreak/>
