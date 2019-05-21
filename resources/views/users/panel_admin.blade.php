e04000b5166d154784fcc901a00bdcc90685f7f6
Marvin <marvinlink07@gmail.com>
2019-05-21 09:31:44 -0600

    admin

--- resources/views/users/panel_admin.blade.php
+++ resources/views/users/panel_admin.blade.php
@@ -156,12 +156,13 @@
                                     </a>
                                     <a class="list-group-item list-group-item-action" style="color:black" href="#">
                                             <li class="list-group-item">
-                                                <div class="md-v-line"></div><i class="fas fa-user mr-5"></i>Acumulativos
+                                                <div class="md-v-line">
+                                                <i class="fas fa-edit mr-5"></i>Acumulativos
                                                   <button><a href="">IP</a></button>
                                                   <button><a href="">IIP</a></button>
                                                   <button><a href="">IIIP</a></button>
                                                   <button><a href="">IVP</a></button>
-                                            </li>
+                                            </li></div>
                                     </a>
                                     <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('users_boletas/{course_id}/{section}', [$asignacion->course_id,$asignacion->section])}} ">
                                         <li class="list-group-item">
