3e2bc9aba93c61bea76422efe09cae4564cf7889
Marvin <marvinlink07@gmail.com>
2019-05-21 10:49:16 -0600

    admin

--- resources/views/users/panel_admin.blade.php
+++ resources/views/users/panel_admin.blade.php
@@ -144,11 +144,7 @@
                         <div class="card-body card-body-cascade ">
                                         
                             <ul class="list-group">
-                                    <a class="list-group-item list-group-item-action" style="color:black" href="#">
-                                        <li class="list-group-item">
-                                            <div class="md-v-line"></div><i class="fas fa-user mr-5"></i>Panel
-                                        </li>
-                                    </a>
+                                   
                                     <a class="list-group-item list-group-item-action" style="color:black" href="#">
                                             <li class="list-group-item">
                                                 <div class="md-v-line"></div><i class="fas fa-user mr-5"></i>Estudiantes
