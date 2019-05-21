b1856b15f32b607b74bb626d24e173fa40527e67
Marvin <marvinlink07@gmail.com>
2019-05-21 10:42:16 -0600

    admin

--- resources/views/users/panel_admin.blade.php
+++ resources/views/users/panel_admin.blade.php
@@ -159,10 +159,10 @@
                                                 <div class="md-v-line"></div>
                                                  <i class="fas fa-user mr-5"></i>Acumulativos
                                                  <a href=""><div class="list-group-item">
-                                                   <button style="width:40px; color:black" class="btn btn-sm" href="">I P</button >
-                                                   <button style="width:40px; color:black" class="btn btn-sm" href="">II P</button >
-                                                   <button style="width:40px; color:black" class="btn btn-sm" href="">III P</button >
-                                                   <button style="width:40px; color:black" class="btn btn-sm" href="">IV P</button >
+                                                   <button style="width:90px;" class="btn btn-sm btn-primary" href="">I P</button >
+                                                   <button style="width:90px;" class="btn btn-sm btn-primary" href="">II P</button >
+                                                   <button style="width:90px;" class="btn btn-sm btn-primary" href="">III P</button >
+                                                   <button style="width:90px;" class="btn btn-sm btn-primary" href="">IV P</button >
                                                   </div>
                                                 </a>
                                             </li>
