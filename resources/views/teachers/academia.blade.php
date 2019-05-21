9bd53788b6c38e42a4e5738f0b734d1deb4d06cf
Marvin <marvinlink07@gmail.com>
2019-05-20 20:19:45 -0600

    academia on

--- resources/views/teachers/academia.blade.php
+++ resources/views/teachers/academia.blade.php
@@ -214,13 +214,13 @@
                                  <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('teachers/acumulativos/{user_id}/{course_id}/{section}/{clase}/{parcial}', [$asignacion->user_id,$asignacion->course_id,$asignacion->section,$asignacion->clase_id,2])}} "> 
                                 {{--<a class="list-group-item list-group-item-action" style="color:black" href="#">--}}
                                     <li class="list-group-item">
-                                        <div class="md-v-line"></div><i class="fas fa-edit mr-5"></i>Acumulativos (II P)
+                                        <div class="md-v-line"></div><i class="fas fa-edit mr-5"></i>Acumulativos
                                     </li>
                                 </a>
-                                {{-- <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('teachers/examen/{user_id}/{course_id}/{section}/{clase}', [$asignacion->user_id,$asignacion->course_id,$asignacion->section,$asignacion->clase_id])}} "> --}}
-                                  <a class="list-group-item list-group-item-action" style="color:black" href="#">
+                                 <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('teachers/examen/{user_id}/{course_id}/{section}/{clase}', [$asignacion->user_id,$asignacion->course_id,$asignacion->section,$asignacion->clase_id])}} "> 
+                                  {{--<a class="list-group-item list-group-item-action" style="color:black" href="#">--}}
                                     <li class="list-group-item">
-                                        <div class="md-v-line"></div><i class="fas fa-user-edit mr-5"></i>Examen
+                                        <div class="md-v-line"></div><i class="fas fa-user-edit mr-5"></i>Notas
                                     </li>
                                 </a>
                                {{--  <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('teachers/descargas/{user_id}/{course_id}/{section}/{clase}', [$asignacion->user_id,$asignacion->course_id,$asignacion->section,$asignacion->clase_id])}} ">--}}
