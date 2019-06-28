7b21d12f8700eb269aba1c8ecc68bee461e8eab8
Marvin <marvinlink07@gmail.com>
2019-06-28 11:23:10 -0600

    IP

--- resources/views/teachers/examen.blade.php
+++ resources/views/teachers/examen.blade.php
@@ -316,8 +316,8 @@
                                      @foreach ($students as $student)
                                         <tr>
                                             <td>{{$student->name}} {{$student->lastname}}</td>
-                                            <td><input maxlength="2"  onchange="calcular_semestre1({{$student->user_id}})" type="text" name='acum1_{{$student->user_id}}' id='acum1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum1}}"></td>
-                                            <td><input maxlength="2"  onchange="calcular_semestre1({{$student->user_id}})"type="text" name='exa1_{{$student->user_id}}' id='exa1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa1}}"></td>
+                                            <td><input maxlength="2" readonly onchange="calcular_semestre1({{$student->user_id}})" type="text" name='acum1_{{$student->user_id}}' id='acum1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum1}}"></td>
+                                            <td><input maxlength="2" readonly onchange="calcular_semestre1({{$student->user_id}})"type="text" name='exa1_{{$student->user_id}}' id='exa1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa1}}"></td>
                                             <td><input maxlength="2" readonly type="text" name='total1_{{$student->user_id}}' id='total1_{{$student->user_id}}' min="0" required class="form-control" value="{{ ($student->Acum1) + ($student->Exa1)}}"></td>
                                            
                                             <td><input maxlength="2"  onchange="calcular_semestre2({{$student->user_id}})"type="text" name='acum2_{{$student->user_id}}' id='acum2_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum2}}"></td>
