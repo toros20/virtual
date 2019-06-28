969cd30466a7f7695bfbda3a88c14f158577b90a
Marvin <marvinlink07@gmail.com>
2019-06-28 11:05:47 -0600

    acta

--- resources/views/users/actas.blade.php
+++ resources/views/users/actas.blade.php
@@ -173,8 +173,8 @@
                                             ->where ([
                                                         [$historial.'.clase_id', '=', $clase->clase_id],
                                                         [$historial.'.student_id', '=', $estudiante->user_id],
-                                                        [$historial.'.clase_id', '!=',50],
-                                                        [$historial.'.clase_id', '!=',57],
+                                                        [$historial.'.clase_id', '<>',50],
+                                                        [$historial.'.clase_id', '<>',57],
                                                     ])
                                             ->Select($historial.'.*')
                                             ->get();
