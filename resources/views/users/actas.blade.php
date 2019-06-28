c580c2f602e6011ab634612db4886119debce884
Marvin <marvinlink07@gmail.com>
2019-06-28 11:11:41 -0600

    acta

--- resources/views/users/actas.blade.php
+++ resources/views/users/actas.blade.php
@@ -174,7 +174,7 @@
                                                         [$historial.'.clase_id', '=', $clase->clase_id],
                                                         [$historial.'.student_id', '=', $estudiante->user_id],
                                                         [$historial.'.clase_id', '<>',50],
-                                                        [$historial.'.clase_id', '<>',57],
+                                                       
                                                     ])
                                             ->Select($historial.'.*')
                                             ->get();
