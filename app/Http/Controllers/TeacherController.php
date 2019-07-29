de2f8b23089aa2b30796217059e98ec83461899c
Marvin <marvinlink07@gmail.com>
2019-07-29 14:41:02 -0600

    1sem

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -118,14 +118,14 @@ class TeacherController extends Controller
         $user = User::findOrFail($user_id);
 
          //obtenemos las asignaciones de este docentes
-         /*$asignaciones = DB::table('assignments')
+         $asignaciones = DB::table('assignments')
                         ->join('courses', 'assignments.course_id', '=', 'courses.id')
                         ->join('clases', 'assignments.clase_id', '=', 'clases.id')
                         ->Select('assignments.user_id','courses.id as course_id','clases.id as clase_id','courses.short_name as course','clases.short_name as clase','assignments.section','courses.modality_id')
                         ->where('assignments.user_id','=',$user_id)
-                        ->get(); */
+                        ->get(); 
 
-         $asignaciones = DB::table('assignments')
+         /*$asignaciones = DB::table('assignments')
                         ->join('courses', 'assignments.course_id', '=', 'courses.id')
                         ->join('clases', 'assignments.clase_id', '=', 'clases.id')
                         ->Select('assignments.user_id','courses.id as course_id','clases.id as clase_id','courses.short_name as course','clases.short_name as clase','assignments.section','courses.modality_id')
@@ -134,7 +134,7 @@ class TeacherController extends Controller
                             ['assignments.user_id', '=', $user_id],
                             ['clases.semester', '!=', 1],
                         ])
-                        ->get(); 
+                        ->get(); */
 
 
                         //dd($asignaciones);
