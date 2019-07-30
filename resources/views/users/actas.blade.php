2eb7157615762ec80d4c5515195be366699fff9c
Marvin <marvinlink07@gmail.com>
2019-07-30 12:51:09 -0600

    test

--- resources/views/users/actas.blade.php
+++ resources/views/users/actas.blade.php
@@ -143,23 +143,23 @@
             {{-- INICIO PARA CURSOS SEMESTRALES --}}
             @if ($course->is_semestral == 1)
 
-                <table width="100%" style="tabla-striped;  border: 1px solid #dee2e6;">
+                <table width="100%" style="tabla-striped;  border: 1px solid #ccc;">
                   <thead>
                       <?php $historial = 'historial_'.$curso.'_'.$seccion; ?>
                   
                       <?php $cont=0; $cont_clase=1;$total1=0; $total2=0; $total3=0; $total4=0; ?>
 
-                          <tr style="border: 1px solid #dee2e6; ">
-                          <th style="border: 1px solid #dee2e6;">No.</th>
-                          <th  style="border: 1px solid #dee2e6; font-size:14 px;font-weight: bold;">Nombre de Estudiante</th>
+                          <tr style="border: 1px solid #ccc; ">
+                          <th style="border: 1px solid #ccc;">No.</th>
+                          <th  style="border: 1px solid #ccc; font-size:14 px;font-weight: bold;">Nombre de Estudiante</th>
                           
                           @foreach ($clases as $clase)
-                              <th valign="bottom" style="text-rotate: 90; text-align:center; font-weight: bold; border: 1px solid #dee2e6;">@php echo $cont_clase;  @endphp .{{$clase->short_name}}</th>
-                              <th valign="bottom" style="text-rotate: 90; text-align:center;  border: 1px solid #dee2e6;">RECUPERACION</th>
+                              <th valign="bottom" style="text-rotate: 90; text-align:center; font-weight: bold; border: 1px solid #ccc;">@php echo $cont_clase;  @endphp .{{$clase->short_name}}</th>
+                              <th valign="bottom" style="text-rotate: 90; text-align:center;  border: 1px solid #ccc;">RECUPERACION</th>
                               @php  $cont_clase+=1;   @endphp
                           @endforeach
 
-                          <th style="text-align:center; width:50px;font-weight: bold; border: 1px solid #dee2e6;">Promedio</th>
+                          <th style="text-align:center; width:50px;font-weight: bold; border: 1px solid #ccc;">Promedio</th>
                       </tr>
 
                   </thead>
@@ -169,8 +169,8 @@
                           $promedio=0;
                       @endphp
                       <tr>
-                          <td style="font-size:14px; width:20px; border: 1px solid #dee2e6; text-align:left;"><?php echo $cont+1; ?></td>
-                          <td style="font-size:14px; border: 1px solid #dee2e6; text-align:left;"><p> {{$estudiante->name}} {{$estudiante->lastname}}</p></td>
+                          <td style="font-size:14px; width:20px; border: 1px solid #ccc; text-align:left;"><?php echo $cont+1; ?></td>
+                          <td style="font-size:14px; border: 1px solid #ccc; text-align:left;"><p> {{$estudiante->name}} {{$estudiante->lastname}}</p></td>
                         
                           @foreach ($clases as $clase)
                               <?php 
@@ -206,21 +206,21 @@
                                   
                               ?>
                               @if ( $total < 70)
-                                  <td style="font-size:14px ;text-align:center; width:30px; border: 1px solid #dee2e6; color:red"><?php echo $total ?> </td>
+                                  <td style="font-size:14px ;text-align:center; width:30px; border: 1px solid #ccc; color:red"><?php echo $total ?> </td>
                               @else 
-                                  <td style="font-size:14px ;text-align:center; width:30px; border: 1px solid #dee2e6;"><?php echo $total ?> </td>
+                                  <td style="font-size:14px ;text-align:center; width:30px; border: 1px solid #ccc;"><?php echo $total ?> </td>
                               @endif
 
                               @if ( $recuperacion < 70 and $recuperacion  > 0)
-                              <td style="font-size:14px ;text-align:center; width:30px; border: 1px solid #dee2e6; color:red"><?php echo $recuperacion ?> </td>
+                              <td style="font-size:14px ;text-align:center; width:30px; border: 1px solid #ccc; color:red"><?php echo $recuperacion ?> </td>
                               @endif
 
                               @if ($recuperacion > 69)
-                              <td style="font-size:14px ;text-align:center; width:30px; border: 1px solid #dee2e6;"><?php echo $recuperacion ?> </td>                                
+                              <td style="font-size:14px ;text-align:center; width:30px; border: 1px solid #ccc;"><?php echo $recuperacion ?> </td>                                
                               @endif
 
                               @if ($recuperacion ==0)
-                              <td style="font-size:14px ;text-align:center; width:30px; border: 1px solid #dee2e6;"></td>                                
+                              <td style="font-size:14px ;text-align:center; width:30px; border: 1px solid #ccc;"></td>                                
                               @endif
                                   
                           @endforeach {{--fin del ciclo para cada clase --}}
@@ -231,9 +231,9 @@
                           @endphp
 
                           @if ( $promedio_final < 70)
-                              <td style="font-size:14px ;text-align:center; width:50px; border: 1px solid #dee2e6; color:red"><?php echo $promedio_final ?> </td>
+                              <td style="font-size:14px ;text-align:center; width:50px; border: 1px solid #ccc; color:red"><?php echo $promedio_final ?> </td>
                           @else 
-                              <td style="font-size:14px ;text-align:center; width:50px; border: 1px solid #dee2e6;"><?php echo $promedio_final ?> </td>
+                              <td style="font-size:14px ;text-align:center; width:50px; border: 1px solid #ccc;"><?php echo $promedio_final ?> </td>
                           @endif
                           
                       </tr>
