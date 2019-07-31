95d8a2c9e9b56fcb6e5ebc2dc01a6c9673f26e4e
Marvin <marvinlink07@gmail.com>
2019-07-31 09:39:32 -0600

    test

--- resources/views/users/boletas_docentes.blade.php
+++ resources/views/users/boletas_docentes.blade.php
@@ -261,7 +261,7 @@ $html = ob_get_clean();
 //$mpdf=new mPDF('c','Letter','',''); 
 $mpdf=new \Mpdf\Mpdf([
     'mode' => 'utf-8',
-    'format' => [190, 236],
+    'format' => [216, 279],
     'orientation' => 'P'
 ]); 
 
