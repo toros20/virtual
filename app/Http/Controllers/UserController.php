b966299d37b1b0a6af024b62343d94b0a9b7c31d
Marvin <marvinlink07@gmail.com>
2019-09-02 11:42:05 -0600

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1244,7 +1244,7 @@ public function verificar_cuenta(Request $request){
        $control = DB::table('control_encuesta')
                         ->where('estudiante','=',$usuario[0]->id)
                         ->get();
-
+        dd($control);
        switch ($control->pregunta) {
             case '0':
                    //obtenemos la pregunta numero 1 de la base de datos
