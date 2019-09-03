8cbf0dddf01c76c84f0eab347a0655c7cd25b5df
Marvin <marvinlink07@gmail.com>
2019-09-03 09:54:54 -0600

    test

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -759,7 +759,7 @@
                                     <tr><td>&nbsp;</td><td>&nbsp;</td> <td>&nbsp;</td></tr> 
                                     <tr><td>&nbsp;</td><td>&nbsp;</td> <td>&nbsp;</td></tr> 
                                     <tr><td>&nbsp;</td><td>&nbsp;</td> <td>&nbsp;</td></tr> 
-                                    <tr><td>&nbsp;</td><td>&nbsp;</td> <td>&nbsp;</td></tr> 
+                                    
                                                                
                                 </table>
                             </div>
