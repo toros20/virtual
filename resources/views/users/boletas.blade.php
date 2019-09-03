ddefaee86521c0264dcc6f0fce90aaff13059a33
Marvin <marvinlink07@gmail.com>
2019-09-03 08:25:50 -0600

    text

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -133,7 +133,7 @@
                                                     ->get();
                                             $total1+=($resultado[0]->Acum1) + ($resultado[0]->Exa1);
                                             $total2+=($resultado[0]->Acum2) + ($resultado[0]->Exa2);
-                                            //$total3+=($resultado[0]->Acum3) + ($resultado[0]->Exa3);
+                                            $total3+=($resultado[0]->Acum3) + ($resultado[0]->Exa3);
                                             //$total4+=($resultado[0]->Acum4) + ($resultado[0]->Exa4);
                                         
                                     ?>
