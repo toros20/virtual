2c1c01ca38230856d3b575b82b5a56c46ed03116
Marvin <marvinlink07@gmail.com>
2019-09-03 12:48:52 -0600

    test

--- resources/views/users/realizar_encuesta.blade.php
+++ resources/views/users/realizar_encuesta.blade.php
@@ -192,6 +192,29 @@
             return false;
         });
     });
+
+
+    function copyToClipboard() {
+    // Create a "hidden" input
+    var aux = document.createElement("input");
+    // Assign it the value of the specified element
+    aux.setAttribute("value", "Tambien se Prohibe realizar Capturas de Pantallas. JAJAJA");
+    // Append it to the body
+    document.body.appendChild(aux);
+    // Highlight its content
+    aux.select();
+    // Copy the highlighted text
+    document.execCommand("copy");
+    // Remove it from the body
+    document.body.removeChild(aux);
+    alert("Tambien se Prohibe realizar Capturas de Pantallas. JAJAJA");
+    }
+
+    $(window).keyup(function(e){
+    if(e.keyCode == 44){
+        copyToClipboard();
+    }
+    }); 
     </script>
 </body>
 
