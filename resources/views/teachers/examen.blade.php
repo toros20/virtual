beb5a7cc54673a8b160ecf5130ad5dce19bbe377
Marvin <marvinlink07@gmail.com>
2019-09-13 14:00:09 -0600

    test

--- resources/views/teachers/examen.blade.php
+++ resources/views/teachers/examen.blade.php
@@ -315,7 +315,7 @@
                                 <tbody id="tbody1" >
                                      @foreach ($students as $student)
                                         <tr>
-                                          {{--   <td>{{$student->name}} {{$student->lastname}}</td>
+                                             <td>{{$student->name}} {{$student->lastname}}</td>
                                             <td><input maxlength="2"  onchange="calcular_semestre1({{$student->user_id}})" type="text" name='acum1_{{$student->user_id}}' id='acum1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum3}}"></td>
                                             <td><input maxlength="2"  onchange="calcular_semestre1({{$student->user_id}})"type="text" name='exa1_{{$student->user_id}}' id='exa1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa3}}"></td>
                                             <td><input maxlength="2" readonly type="text" name='total1_{{$student->user_id}}' id='total1_{{$student->user_id}}' min="0" required class="form-control" value="{{ ($student->Acum3) + ($student->Exa3)}}"></td>
@@ -326,11 +326,11 @@
                                              
                                             <td><input maxlength="2" readonly type="text" name='promedio_{{$student->user_id}}' id='promedio_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Promedio}}" ></td>
                                             <td><input maxlength="3" type="text" name='recu_{{$student->user_id}}' id='recu_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Recu1}}" ></td>
-                                             --}}{{-- <td><input maxlength="2" disabled type="text" name='recu2_{{$student->user_id}}' id='recu2_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Recu2}} "></td> --}}
+                                            <td><input maxlength="2" disabled type="text" name='recu2_{{$student->user_id}}' id='recu2_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Recu2}} "></td> 
                                         
                                         
                                         
-                                            <td>{{$student->name}} {{$student->lastname}}</td>
+                                           {{--PRIMER SEMESTRE  <td>{{$student->name}} {{$student->lastname}}</td>
                                             <td><input maxlength="2"  onchange="calcular_semestre1({{$student->user_id}})" type="text" name='acum1_{{$student->user_id}}' id='acum1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum1}}"></td>
                                             <td><input maxlength="2"  onchange="calcular_semestre1({{$student->user_id}})"type="text" name='exa1_{{$student->user_id}}' id='exa1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa1}}"></td>
                                             <td><input maxlength="2" readonly type="text" name='total1_{{$student->user_id}}' id='total1_{{$student->user_id}}' min="0" required class="form-control" value="{{ ($student->Acum1) + ($student->Exa1)}}"></td>
@@ -341,7 +341,7 @@
                                              
                                             <td><input maxlength="2" readonly type="text" name='promedio_{{$student->user_id}}' id='promedio_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Promedio}}" ></td>
                                             <td><input maxlength="3" type="text" name='recu_{{$student->user_id}}' id='recu_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Recu1}}" ></td>
-                                        
+                                         --}}
                                         
                                         </tr>
                                     @endforeach
