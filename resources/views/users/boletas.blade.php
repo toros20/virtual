4d663cc2b44bf62da012702328f23afa0cb68e5c
Marvin <marvinlink07@gmail.com>
2019-09-17 14:20:45 -0600

    test

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -265,7 +265,7 @@
                             <table class="tabla tabla-striped tabla-bordered"  style=" text-align:center; border: 1px solid #dee2e6; "  align="center" width="700">
                                 <tr style="border: 1px solid #dee2e6; ">
                                     <td ><h4 style="font-weight: bold;"> {{$estudiante->name}} {{$estudiante->lastname}}</h4> </td>
-                                    <td ><h4 style="font-weight: bold;"> II SEMESTRE</h4> </td>
+                                    <td ><h4 style="font-weight: bold;"> I SEMESTRE</h4> </td>
                                 </tr>
                             </table>
                             <table class="tabla tabla-striped tabla-bordered"  style="margin-top:10px; border: 1px solid #dee2e6; "  align="center" width="700">
