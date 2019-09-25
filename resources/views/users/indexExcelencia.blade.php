7ffb8181a0c08b43fcaa127389d39bf8a173ac3a
Marvin <marvinlink07@gmail.com>
2019-09-25 14:31:40 -0600

    test

--- resources/views/users/indexExcelencia.blade.php
+++ resources/views/users/indexExcelencia.blade.php
@@ -112,6 +112,7 @@
 
         <div class="container" style="margin-top: 100px ">
             <h1>Estudiantes de Excelencia Académica</h1>
+            <button class="btn btn-success btn-lg">Nuevo Estudiante</button>
             <div class="row">
                 <table class="table table-striped table-hover">
                     <thead class="thead-dark">
