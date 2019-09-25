f8404027774bec89fa14eeda3f8815d6e866ae87
Marvin <marvinlink07@gmail.com>
2019-09-25 08:13:53 -0600

    font awesome

--- resources/views/students/excelencia.blade.php
+++ resources/views/students/excelencia.blade.php
@@ -7,6 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/3762dfca53.js" crossorigin="anonymous"></script>
+    
     <title>Excelencia Académica 2019</title>
 </head>
 <body style="background-color:#f5f6f5">
