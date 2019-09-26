e95bebecc3ff975724499a77a32fc1c70685e267
Marvin <marvinlink07@gmail.com>
2019-09-26 14:09:38 -0600

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -927,8 +927,8 @@ class UserController extends Controller
                         //->where('clasecourses.course_id','=',$course_id)
                         ->where ([
                             ['clasecourses.course_id', '=', $course_id],
-                            ['clases.semester', '!=', 1],
-                            ['clases.oficial', '!=', 2],
+                            ['clases.semester', '!=', 2],
+                            ['clases.oficial', '!=', 1],
                         ])
                         ->Select('clase_id')
                         ->get(); 
