74416cfb6ec34dda626f509069d7a4f8b9963b82
Marvin <marvinlink07@gmail.com>
2019-09-26 10:47:21 -0600

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1487,7 +1487,7 @@ public function updateExcelencia(Request $request){
 
         //comprobamos que desea cambiar la foto
         $file = $request->file('foto')->store('excelencia');
-        if($file->name != ""){
+        if($file[0]->name != ""){
               $resp =DB::table('excelencias')
                 ->where([
                     ['id', '=', $request->user_id]
