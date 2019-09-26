aa38c809dd580bfd685cc0715e4c5a5d69479906
Marvin <marvinlink07@gmail.com>
2019-09-26 10:13:50 -0600

    test

--- resources/views/users/editExcelencia.blade.php
+++ resources/views/users/editExcelencia.blade.php
@@ -26,7 +26,7 @@
 
                                 {{-- @csrf --}}
                                 <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
-                                <input type="hidden" name="user_id" id="user_id" value="{{$excelencia->id}}">
+                                <input type="hidden" name="user_id" id="user_id" value="{{$excelencia[0]->id}}">
 
                                 <p><label for="cuenta">
                                     Cuenta 
