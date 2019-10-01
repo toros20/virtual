4a0100778e25ec8c4293904e9fdb4580101e2ac5
Marvin <marvinlink07@gmail.com>
2019-10-01 14:37:17 -0600

    test

--- public/js/felicitar.js
+++ public/js/felicitar.js
@@ -1,21 +1,11 @@
 //funcion para felicitar a los estudiantes de excelencia academica
 function felicitar(id) {
 
-    //obtenemos el valor actual de felicitaciones
-    /*var valor_actual = document.getElementById('felicitaciones_'+id).value
-    var nuevo_valor = eval(valor_actual) + 1;
-    document.getElementById("#felicitaciones_".id).value =  valor_actual +1;
-
-    alert("valor inicial="+valor_actual);
-    alert("nuevo valor="+nuevo_valor);*/
-
     var token = $("#token").val();
-    //var token = document.getElementById('token').value
-    alert(token);
+    
     $.ajax({
 
         url: '../../ajax/felicitaciones',
-        //url:'../ajax/coursesbymodalityid',
         headers: token,
         data: { _id: id, _token: token },
         type: 'POST',
