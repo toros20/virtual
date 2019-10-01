847076ff32d49bfe845da39e1fcadbe998b779f2
Marvin <marvinlink07@gmail.com>
2019-10-01 15:00:48 -0600

    test

--- resources/views/ajax/felicitaciones.blade.php
+++ resources/views/ajax/felicitaciones.blade.php
@@ -1,3 +1,3 @@
 {{-- boton actualizado para felicitaciones de excelencia academica --}}
 
-<a id="button_a_{{$excelencia[0]->id}}"  onclick="felicitar({{$excelencia[0]->id}})" class="btn btn-primary btn-block"  style="color:white; ">Felicitaciones ({{$excelencia[0]->felicitaciones}})</a>
+<a  class="btn btn-default btn-block"  style="color:white; ">Muchas Gracias!!! ({{$excelencia[0]->felicitaciones}})</a>
