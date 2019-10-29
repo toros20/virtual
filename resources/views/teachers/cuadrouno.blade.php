4ac074dbe641c7bc6cea631025228c5630413db2
Marvin <marvinlink07@gmail.com>
2019-10-29 09:40:42 -0600

    test

--- resources/views/teachers/cuadrouno.blade.php
+++ resources/views/teachers/cuadrouno.blade.php
@@ -54,7 +54,7 @@
                      </tr>
                      <tr>
                        <td colspan="2" style="font-family: Arial"><strong>MUNICIPIO:DISTRITO CENTRAL</strong></td>
-                     <td colspan="3" style="font-family: Arial"><strong>LUGAR: LA CAMPAÑA     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; I SEMESTRE     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ASIGNATURA:{{$estudiantes[0]->clase}}</strong></td>
+                     <td colspan="3" style="font-family: Arial"><strong>LUGAR: LA CAMPAÑA     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; II SEMESTRE     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ASIGNATURA:{{$estudiantes[0]->clase}}</strong></td>
                    
                      </tr>
                      <tr>
@@ -125,7 +125,7 @@
                             @else
                                 <td style="font-size:14px; width:40px; border: 1px solid #dee2e6; text-align:center;">{{round(($estudiante->Acum3 + $estudiante->Exa3 + $estudiante->Acum4 + $estudiante->Exa4)/2)}}</td>
                             @endif
-                          <img src="" alt="">
+                         
                             @if (($estudiante->Recu1)<70 and ($estudiante->Recu1)>0)
                                  <td style="font-size:14px; width:40px; border: 1px solid #dee2e6; text-align:center;color:red">{{$estudiante->Recu1}}</td>
                             @endif
