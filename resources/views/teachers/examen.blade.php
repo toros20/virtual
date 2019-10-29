d3974607e5328771eb60e3f21c52a7dc36e032a2
Marvin <marvinlink07@gmail.com>
2019-10-29 09:34:47 -0600

    test

--- resources/views/teachers/examen.blade.php
+++ resources/views/teachers/examen.blade.php
@@ -230,8 +230,8 @@
                                             <td><input maxlength="2" readonly onchange="calcular_2P({{$student->user_id}})"type="text" name='exa2_{{$student->user_id}}' id='exa2_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa2}}"></td>
                                             <td><input maxlength="2" readonly type="text" name='total2_{{$student->user_id}}' id='total2_{{$student->user_id}}' min="0" required class="form-control" value="{{ ($student->Acum2) + ($student->Exa2)}}"></td>
                                             
-                                            <td><input maxlength="2"  onchange="calcular_3P({{$student->user_id}})"type="text" name='acum3_{{$student->user_id}}' id='acum3_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum3}}"></td>
-                                            <td><input maxlength="2"  onchange="calcular_3P({{$student->user_id}})"type="text" name='exa3_{{$student->user_id}}' id='exa3_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa3}}"></td>
+                                            <td><input maxlength="2" readonly onchange="calcular_3P({{$student->user_id}})"type="text" name='acum3_{{$student->user_id}}' id='acum3_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum3}}"></td>
+                                            <td><input maxlength="2" readonly onchange="calcular_3P({{$student->user_id}})"type="text" name='exa3_{{$student->user_id}}' id='exa3_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa3}}"></td>
                                             <td><input maxlength="2" readonly type="text" name='total3_{{$student->user_id}}' id='total3_{{$student->user_id}}' min="0" required class="form-control" value="{{ ($student->Acum3) + ($student->Exa3)}}"></td>
                                             
                                             <td><input maxlength="2" readonly onchange="calcular_4P({{$student->user_id}})"type="text" name='acum4_{{$student->user_id}}' id='acum4_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum4}}"></td>
@@ -316,12 +316,12 @@
                                      @foreach ($students as $student)
                                         <tr>
                                              <td>{{$student->name}} {{$student->lastname}}</td>
-                                            <td><input maxlength="2"  onchange="calcular_semestre1({{$student->user_id}})" type="text" name='acum1_{{$student->user_id}}' id='acum1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum3}}"></td>
-                                            <td><input maxlength="2"  onchange="calcular_semestre1({{$student->user_id}})"type="text" name='exa1_{{$student->user_id}}' id='exa1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa3}}"></td>
+                                            <td><input maxlength="2" readonly onchange="calcular_semestre1({{$student->user_id}})" type="text" name='acum1_{{$student->user_id}}' id='acum1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum3}}"></td>
+                                            <td><input maxlength="2" readonly onchange="calcular_semestre1({{$student->user_id}})"type="text" name='exa1_{{$student->user_id}}' id='exa1_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa3}}"></td>
                                             <td><input maxlength="2" readonly type="text" name='total1_{{$student->user_id}}' id='total1_{{$student->user_id}}' min="0" required class="form-control" value="{{ ($student->Acum3) + ($student->Exa3)}}"></td>
                                            
-                                            <td><input maxlength="2"  readonly onchange="calcular_semestre2({{$student->user_id}})"type="text" name='acum2_{{$student->user_id}}' id='acum2_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum4}}"></td>
-                                            <td><input maxlength="2"  readonly onchange="calcular_semestre2({{$student->user_id}})"type="text" name='exa2_{{$student->user_id}}' id='exa2_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa4}}"></td>
+                                            <td><input maxlength="2"   onchange="calcular_semestre2({{$student->user_id}})"type="text" name='acum2_{{$student->user_id}}' id='acum2_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Acum4}}"></td>
+                                            <td><input maxlength="2"   onchange="calcular_semestre2({{$student->user_id}})"type="text" name='exa2_{{$student->user_id}}' id='exa2_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Exa4}}"></td>
                                             <td><input maxlength="2" readonly type="text" name='total2_{{$student->user_id}}' id='total2_{{$student->user_id}}' min="0" required class="form-control" value="{{ ($student->Acum2) + ($student->Exa2)}}"></td>
                                              
                                             <td><input maxlength="2" readonly type="text" name='promedio_{{$student->user_id}}' id='promedio_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Promedio}}" ></td>
