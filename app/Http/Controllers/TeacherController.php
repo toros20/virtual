739cbce027975ffbec9158b90dfc6cfc0a81ed7e
Marvin <marvinlink07@gmail.com>
2019-11-14 07:46:12 -0600

    test

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -127,6 +127,7 @@ class TeacherController extends Controller
                         ->where([
                             ['assignments.user_id', '=', $user_id],
                             ['clases.semester', '!=', 2],
+                            ['clases.oficial', '!=', 2],
                         ])
                         ->get(); 
         
