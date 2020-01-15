3337de848847182031aafca3181b8bad66fdc607
Marvin <marvinlink07@gmail.com>
2020-01-15 09:11:02 -0600

    test

--- resources/views/users/actas.blade.php
+++ resources/views/users/actas.blade.php
@@ -337,7 +337,7 @@
               
             @endif
 
-            @if ($course->id == 9)
+            @if ($course->id == 9 || $course->id == 10)
               <table width="90%" border="0" align="center">
                 <tr>
                   <td align="center">&nbsp;</td>
