5f6e0ecbf0e904fa3f0cecf5291e58e8ce598e5a
Marvin <marvinlink07@gmail.com>
2020-01-20 11:32:23 -0600

    firmas

--- resources/views/users/actas.blade.php
+++ resources/views/users/actas.blade.php
@@ -721,14 +721,14 @@
 
             @if ($course->id >15)
               <table width="90%" border="0" align="center">
-               {{--  <tr>
+                 <tr>
                   <td align="center">&nbsp;</td>
                   <td>&nbsp;</td>
                   <td align="center">&nbsp;</td>
                   <td>&nbsp;</td>
                   <td align="center">&nbsp;</td>
                   <td>&nbsp;</td>
-                </tr> --}}
+                </tr> 
                 <tr>
                   <td width="2%" align="center" style="font-size: 15px">1.</td>
                   <td width="25%" align="center" style="font-size: 15px">________________________________________________</td>
@@ -794,8 +794,8 @@
                     <td width="25%" align="center" style="font-size: 15px">________________________________________________</td>
                     <td width="3%" align="center" style="font-size: 15px">14.</td>
                     <td width="25%" align="center" style="font-size: 15px">________________________________________________</td>
-                    <td width="2%" align="center" style="font-size: 15px">15.</td>
-                    <td width="25%" align="center" style="font-size: 15px">________________________________________________</td>
+                    <td width="2%" align="center" style="font-size: 15px"></td>
+                    <td width="25%" align="center" style="font-size: 15px"></td>
                     <td width="2%" align="center" style="font-size: 15px"></td>
                     <td width="25%" align="center" style="font-size: 15px"></td>
                 </tr>
