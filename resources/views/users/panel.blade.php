6ca75415a013661493340317bb0e70b689987d9f
marvinlin <marvinlink07@gmail.com>
2020-02-13 15:56:03 -0600

    test

--- resources/views/users/panel.blade.php
+++ resources/views/users/panel.blade.php
@@ -151,7 +151,7 @@
                             <ul class="list-group">
                                     <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('list_students/{user_id}/{course_id}/{section}', [$user->id,$asignacion->course_id,$asignacion->section])}} ">
                                         <li class="list-group-item">
-                                            <div class="md-v-line"></div><i class="fas fa-user mr-5"></i>Estudiantes
+                                            <div class="md-v-line"></div><i class="fas fa-user mr-5"></i>Estudiantes2
                                         </li>
                                     </a>
                                     <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('users_boletas/{course_id}/{section}', [$asignacion->course_id,$asignacion->section])}} ">
