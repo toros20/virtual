7119eaf93c4a43659f041d5ef720646b900049af
marvinlin <marvinlink07@gmail.com>
2020-02-14 13:51:03 -0600

    test

--- public/js/main.js
+++ public/js/main.js
@@ -13,8 +13,8 @@ function loadcourses(){
 
     $.ajax({
 
-        url:'../../ajax/coursesbymodalityid', 
-        //url:'../ajax/coursesbymodalityid',
+        //url:'../../ajax/coursesbymodalityid', 
+        url:'../ajax/coursesbymodalityid',
         headers: token ,
         data: {modality_id:modality,_token:token},
         type:'POST',
