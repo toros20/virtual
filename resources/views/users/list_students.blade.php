cc6fdb2a28246d51b600af9b1c443c107ccbc89d
marvinlin <marvinlink07@gmail.com>
2020-02-14 12:05:34 -0600

    test

--- resources/views/users/list_students.blade.php
+++ resources/views/users/list_students.blade.php
@@ -9,8 +9,8 @@
     </head>
     <body>
 
-        <h1>Listado de Estudiantes o <a href="{{route('users.students.create')}} ">crear nuevo estudiante</a></h1>
-        <h3>{{$curso->short_name}}-{{$section}}</h3>
+        <h1>Listado de Estudiantes de {{$curso->short_name}}-{{$section}} <a href="{{route('users.students.create')}} ">crear nuevo estudiante</a></h1>
+     
         <h3><a href="{{route('users_panel_admin/{user_id}', $usuario->id)}} ">Volver al Panel</a></h3>
         <table>
             <thead>
