7c582e65950a7e0eddf3580ba8967ca8f999dba4
marvinlin <marvinlink07@gmail.com>
2020-02-21 07:39:35 -0600

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -114,14 +114,14 @@ class UserController extends Controller
         
          //codigo para insertar filas en la tabla historial, por cada curso y section, 
          //se requiere como para metros course, section
-        $clases = Clasecourse::where('course_id', '=', $course)->Select('clase_id')->get();
+        //$clases = Clasecourse::where('course_id', '=', $course)->Select('clase_id')->get();
          
         /*$clases = Clasecourse::where([
             ['course_id', '=', $course]
             //['clase_id', '>', 188],
         ])->Select('clase_id')->get();*/
 
-        $users = Enrollment::where([
+        /*$users = Enrollment::where([
             ['course_id', '=', $course],
             ['section', '=', $section],
         ])->Select('user_id')->get();
@@ -135,7 +135,7 @@ class UserController extends Controller
            }
         }
         
-        return "LISTO---Filas Insertadas De:" . $course ."-". $section ;
+        return "LISTO---Filas Insertadas De:" . $course ."-". $section ;*/
 
 
         
