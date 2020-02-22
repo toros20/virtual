07067020918a9e90c5ba6a9183023666f29fd874
marvinlin <marvinlink07@gmail.com>
2020-02-22 09:57:22 -0600

    test

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -179,8 +179,21 @@ class TeacherController extends Controller
                     ])
                     ->Select('users.name','users.lastname','users.id','users.cuenta')
                     ->get();
+
+         //obtenemos las asignaciones de este docentes
+         $asignaciones = DB::table('assignments')
+                        ->join('courses', 'assignments.course_id', '=', 'courses.id')
+                        ->join('clases', 'assignments.clase_id', '=', 'clases.id')
+                        ->Select('assignments.user_id','courses.id as course_id','clases.id as clase_id','courses.short_name as course','clases.short_name as clase','assignments.section','courses.modality_id')
+                        //->where('assignments.user_id','=',$user_id)
+                        ->where([
+                            ['assignments.user_id', '=', $user_id],
+                            ['clases.semester', '!=', 2],
+
+                        ])
+                        ->get(); 
         
-        return view('teachers/students',compact('students'));
+        return view('teachers/students',compact('students','asignaciones'));
 
 
      }
