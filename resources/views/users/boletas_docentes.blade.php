5fcbe07c892cccc98d81aa947fe4613de9bb7ec6
marvinlin <marvinlink07@gmail.com>
2020-03-06 13:28:05 -0600

    test

--- resources/views/users/boletas_docentes.blade.php
+++ resources/views/users/boletas_docentes.blade.php
@@ -191,8 +191,8 @@
                                     <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;"><?php echo $tea;?></td>
                                    
                                     <td style="text-align:center; width:50px ;padding:0.35rem; border: 1px solid #dee2e6;"><?php echo round(($tea*100)/$total_evaluados);?> %</td> 
-                                    <td style="text-align:center; width:50px ;padding:0.35rem; border: 1px solid #dee2e6;"></td> @php dd($tea.'-'.$total_evaluados);@endphp
-                                    <td style="text-align:center; width:50px ;padding:0.35rem; border: 1px solid #dee2e6; color:red;"><?php echo $ter;?></td>
+                                    <td style="text-align:center; width:50px ;padding:0.35rem; border: 1px solid #dee2e6;"></td> 
+                                    <td style="text-align:center; width:50px ;padding:0.35rem; border: 1px solid #dee2e6; color:red;"><?php echo $ter;?></td>@php dd($tea.'-'.$total_evaluados);@endphp
                                     <td style="text-align:center; width:50px ;padding:0.35rem; border: 1px solid #dee2e6; color:red;"><?php echo round(($ter*100)/$total_evaluados);?> %</td>
                             </tr>
                         </table>
