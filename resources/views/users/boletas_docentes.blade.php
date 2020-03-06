b573dd99513e86416255426a97a5d33f4c7638eb
marvinlin <marvinlink07@gmail.com>
2020-03-06 12:51:47 -0600

    test

--- resources/views/users/boletas_docentes.blade.php
+++ resources/views/users/boletas_docentes.blade.php
@@ -138,7 +138,7 @@
                                                             ])
                                                     ->Select($historial.'.Acum1', $historial.'.Exa1')
                                                     ->get();
-                                    dd($resultados);
+                                    dd($asignaciones);
                         
                                     foreach ($resultados as $resultado) {
                                         $total_alumnos+=1;$total_evaluados+=1;
