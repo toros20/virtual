91e41bf34c5a4efa2fb414240d6f1b56a3c72db6
marvinlin <marvinlink07@gmail.com>
2020-03-09 11:18:38 -0600

    test

--- resources/views/teachers/acumulativos.blade.php
+++ resources/views/teachers/acumulativos.blade.php
@@ -487,9 +487,9 @@
                                              <td><button onclick="evaluar_task({{$task->id}},{{$curso_actual[0]->id}},'{{$section_actual}}')" type="button" class="btn btn-info btn-rounded btn-sm m-0">Evaluar</button></td> 
                                             {{--<td><button onclick="#" type="button" class="btn btn-info btn-rounded btn-sm m-0">Evaluar</button></td>--}}
                                             
-                                            <td><button type="button" class="btn btn-warning btn-rounded btn-sm m-0">Editar</button></td>
+                                             <td><button onclick="editar_task({{$task->id}},{{$curso_actual[0]->id}},'{{$section_actual}}')" type="button" class="btn btn-warning btn-rounded btn-sm m-0">Editar</button></td>
                                             <td><button onclick="eliminar_task({{$task->id}},{{$curso_actual[0]->id}},'{{$section_actual}}')" type="button" class="btn btn-danger btn-rounded btn-sm m-0">Eliminar</button></td>
-                                            <td><button onclick="editar_task({{$task->id}},{{$curso_actual[0]->id}},'{{$section_actual}}')" type="button" class="btn btn-warning btn-rounded btn-sm m-0">Editar</button></td>
+                                           
                                         @else 
                                             @php $total_evaluado+=$task->valor;@endphp
                                             <td><button onclick="evaluar_task({{$task->id}},{{$curso_actual[0]->id}},'{{$section_actual}}')" type="button" class="btn btn-success btn-rounded btn-sm m-0">Evaluada</button></td>
