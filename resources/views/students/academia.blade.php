0e9bb4b8a3390c695b2ea9c4399ea5cb0f0fb847
marvinlin <marvinlink07@gmail.com>
2020-03-09 08:08:30 -0600

    test

--- resources/views/students/academia.blade.php
+++ resources/views/students/academia.blade.php
@@ -193,11 +193,11 @@
                       @if ($asignaciones[0]->is_semestral == 1)
                       <ul class="list-group">
 
-                         {{-- <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('users/boleta_acumulativos/{course_id}/{section}/{user_id}', [$enroll[0]->course_id,$enroll[0]->section,$user->id])}} ">
+                          <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('users/boleta_acumulativos/{course_id}/{section}/{user_id}', [$enroll[0]->course_id,$enroll[0]->section,$user->id])}} ">
                               <li class="list-group-item">
                                   <div class="md-v-line"></div><i class="fas fa-star mr-5"></i>Boleta de Acumulativos
                               </li>
-                          </a> --}}
+                          </a> 
 
                           <a class="list-group-item list-group-item-action" style="color:black" href="{{ $url = route('students/acumulativos/{user_id}/{clase}/{parcial}', [$user->id,$clase,1])}} ">
                               <li class="list-group-item">
