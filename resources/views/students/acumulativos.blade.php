74f5f183a42c496025cb09d32f5a96e2ccecb695
marvinlin <marvinlink07@gmail.com>
2020-03-15 11:12:26 -0600

    test

--- resources/views/students/acumulativos.blade.php
+++ resources/views/students/acumulativos.blade.php
@@ -200,7 +200,7 @@
                                                 ['clase_id', '=', $clase ],
                                                 ['task_id', '=', $task->id ]                    
                                             ])
-                                            ->count('id');
+                                            ->count(DB::raw('DISTINCT id'))
                                            
                                            dd($filetask) 
                                       //$num_files = $filetask;
