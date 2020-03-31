42b88363b3f44ab25e195c876960f839689e2477
marvinlin <marvinlink07@gmail.com>
2020-03-30 18:08:07 -0600

    view

--- resources/views/users/boleta_acumulativos.blade.php
+++ resources/views/users/boleta_acumulativos.blade.php
@@ -180,7 +180,7 @@
                                             {{-- I PARCIAL --}}
                                              
                                             <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;">{{($resultado[0]->Acum1) + ($resultado[0]->Exa1) . '/' . $valor}}</td>
-                                            @php dd({{$resultado[0]->Acum1) + ($resultado[0]->Exa1) . '/' . $valor}}); @endphp
+                                            @php dd($resultado[0]->Acum1) + ($resultado[0]->Exa1) . '/' . $valor); @endphp
                                             
                                             {{-- II PARCIAL --}}
                                            {{--  @if ( ($resultado[0]->Acum2) + ($resultado[0]->Exa2) < 70)
