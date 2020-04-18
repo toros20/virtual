fa6b5861df517378eb851f2229443ee9feca283b
marvinlin <marvinlink07@gmail.com>
2020-04-18 00:53:46 -0600

    image

--- resources/views/teachers/panel.blade.php
+++ resources/views/teachers/panel.blade.php
@@ -572,8 +572,8 @@
 						
 							</div>
 							@if ($mensaje->tipo != 'section')
-								<a href="{{ URL::asset('../storage/app/'.$file->filename)}}">
-									<img src="{{ URL::asset('../storage/app/'.$file->filename)}}" >
+								<a href="{{ URL::asset('../storage/app/'.$mensaje->filename)}}">
+									<img src="{{ URL::asset('../storage/app/'.$mensaje->filename)}}" >
 								</a>
 							@endif
 							<p> {{$mensaje->mensaje}}</p>
