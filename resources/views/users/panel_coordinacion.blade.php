679b96edcf364f6ea0f1b97987c9e04d92556461
marvinlin <marvinlink07@gmail.com>
2020-06-08 00:06:48 -0600

    fix

--- resources/views/users/panel_coordinacion.blade.php
+++ resources/views/users/panel_coordinacion.blade.php
@@ -171,7 +171,7 @@
                                              ['clases.semester', '<',2 ],
                                             ['assignments.section', '=', $seccion->section ]                        
                                         ])
-                                        ->Select('users.name','users.lastname','users.id','clases.short_name','clases.id as clase_id')
+                                        ->Select('users.name','users.lastname','users.id as teacher_id','clases.short_name','clases.id as clase_id')
                                         ->distinct()
                                         ->get(); 
                                         
@@ -227,7 +227,7 @@
                                 ?>
                                 <tr>
                                   <th scope="row">1</th>
-                                  <td><a ></a> Lic. {{$asignacion->name}}  {{$asignacion->lastname}}</td>
+                                  <td><a href="{{ $url = route('teachers/acumulativos/{user_id}/{course_id}/{section}/{clase}/{parcial}', [$asignacion->teacher_id,$seccion->course_id,$seccion->section,$asignacion->clase_id,2])}} "> Lic. {{$asignacion->name}}  {{$asignacion->lastname}}</a></td>
                                   <td>{{$asignacion->short_name}}</td>
 
                                   <?php 
