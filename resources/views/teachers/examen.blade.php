8d72ec408baee744ad465209ba59fed23397dd43
marvinlin <marvinlink07@gmail.com>
2020-06-30 15:58:32 -0600

    fix recuperacion

--- resources/views/teachers/examen.blade.php
+++ resources/views/teachers/examen.blade.php
@@ -325,7 +325,7 @@
                                             <td><input maxlength="2" readonly type="text" name='total2_{{$student->user_id}}' id='total2_{{$student->user_id}}' min="0" required class="form-control" value="{{ ($student->Acum2) + ($student->Exa2)}}"></td>
                                              
                                             <td><input maxlength="2" readonly type="text" name='promedio_{{$student->user_id}}' id='promedio_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Promedio}}" ></td>
-                                            <td><input maxlength="3" readonly type="text" name='recu_{{$student->user_id}}' id='recu_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Recu1}}" ></td>
+                                            <td><input maxlength="3"  type="text" name='recu_{{$student->user_id}}' id='recu_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Recu1}}" ></td>
                                             <td><input maxlength="2" disabled type="text" name='recu2_{{$student->user_id}}' id='recu2_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Recu2}} "></td> 
             
                                         
@@ -340,8 +340,8 @@
                                             <td><input maxlength="2" readonly type="text" name='total2_{{$student->user_id}}' id='total2_{{$student->user_id}}' min="0" required class="form-control" value="{{ ($student->Acum2) + ($student->Exa2)}}"></td>
                                              
                                            <td><input maxlength="2" readonly type="text" name='promedio_{{$student->user_id}}' id='promedio_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Promedio}}" ></td>
-                                             --}}<td><input maxlength="3" type="text" name='recu_{{$student->user_id}}' id='recu_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Recu1}}" ></td>
-                                         
+                                            <td><input maxlength="3" type="text" name='recu_{{$student->user_id}}' id='recu_{{$student->user_id}}' min="0" required class="form-control" value="{{$student->Recu1}}" ></td>
+                                          --}}
                                         
                                         </tr>
                                     @endforeach
