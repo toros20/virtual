7f6c227c8634675923546266d3306d647c23716c
marvinlin <marvinlink07@gmail.com>
2020-07-21 14:16:41 -0600

    fix

--- resources/views/users/boletas.blade.php
+++ resources/views/users/boletas.blade.php
@@ -228,16 +228,20 @@
                                            
                                                {{-- III PARCIAL --}}
                                           @if ( ($resultado[0]->Acum3) + ($resultado[0]->Exa3) < 70)
-                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; color:red; padding:0.35rem;">{{($resultado[0]->Acum3) + ($resultado[0]->Exa3)}}</td>
+                                            {{-- <td style="border: 1px solid #dee2e6; text-align:center; width:50px; color:red; padding:0.35rem;">{{($resultado[0]->Acum3) + ($resultado[0]->Exa3)}}</td> --}}
+                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; color:red; padding:0.35rem;"></td>
                                          @else 
-                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;">{{($resultado[0]->Acum3) + ($resultado[0]->Exa3)}}</td>
+                                            {{-- <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;">{{($resultado[0]->Acum3) + ($resultado[0]->Exa3)}}</td> --}}
+                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;"></td>
                                          @endif
 
                                          {{-- IV PARCIAL --}}
                                          @if ( ($resultado[0]->Acum4) + ($resultado[0]->Exa4) < 70)
-                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem; color:red;">{{($resultado[0]->Acum4) + ($resultado[0]->Exa4)}}</td>
+                                            {{-- <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem; color:red;">{{($resultado[0]->Acum4) + ($resultado[0]->Exa4)}}</td> --}}
+                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem; color:red;"></td>
                                         @else 
-                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;">{{($resultado[0]->Acum4) + ($resultado[0]->Exa4)}}</td>
+                                            {{-- <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;">{{($resultado[0]->Acum4) + ($resultado[0]->Exa4)}}</td> --}}
+                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;"></td>
                                         @endif
 
                                         @php
@@ -253,9 +257,9 @@
                                     
                                         {{-- PROMEDIO FINAL --}}
                                         @if (round($PROM_FINAL) < 70 )
-                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem; color:red;"><?php echo round($PROM_FINAL)?></td>
+                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem; color:red;"><?php //echo round($PROM_FINAL)?></td>
                                         @else
-                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;"><?php echo round($PROM_FINAL)?></td>
+                                            <td style="border: 1px solid #dee2e6; text-align:center; width:50px; padding:0.35rem;"><?php //echo round($PROM_FINAL)?></td>
                                         @endif
 
             
