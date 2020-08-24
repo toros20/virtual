60270c84457a0f54511fc5ee731d41be336ddf52
marvinlin <marvinlink07@gmail.com>
2020-08-23 18:41:21 -0600

    test

--- resources/views/users/gsuit.blade.php
+++ resources/views/users/gsuit.blade.php
@@ -63,9 +63,6 @@
             }
         </style>
         
-     <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.min.js""></script>
-    <script type="text/javascript" language="javascript" src="{{ URL::asset('js/main.js')}}"></script>  
-
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -132,6 +129,8 @@
                
             </div>
         </div>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.min.js""></script>
+    <script type="text/javascript" language="javascript" src="{{ URL::asset('js/main.js')}}"></script>  
+
     </body>
 </html>
