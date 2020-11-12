9c80555e6f599c1e2e69a99c97a117e9cf7022bb
marvinlin <marvinlink07@gmail.com>
2020-11-12 09:33:01 -0600

    primer semestre

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -1078,8 +1078,8 @@ class TeacherController extends Controller
                             ->join('clases', $tabla.'.clase_id', '=', 'clases.id')
                             ->where ([
                                         [$tabla.'.clase_id', '=', $clase],
-                                        ['clases.semester', '!=', 1],
-                                       // ['clases.semester', '!=', 2],
+                                        ['clases.semester', '!=', 2],
+                                       // ['clases.semester', '!=', 1],
                                     ])
                             ->Select('users.name','users.lastname','users.id as user_id','users.sexo',$tabla.'.*','clases.name as clase')
                             ->orderBy('users.sexo','asc')
